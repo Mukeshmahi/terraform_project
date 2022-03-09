215aafd9dfe1b655d2db9265919f28c7e368b49f
Mukeshmahi <64925113+Mukeshmahi@users.noreply.github.com>
2022-03-10 00:57:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,27 +3,12 @@ provider "aws" {
   access_key = "AKIAQHB325YWIUXIZZNC"
   secret_key = "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"
 }
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami           = "ami-0e0ff68cb8e9a188a"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "TEST"
   }
 }
